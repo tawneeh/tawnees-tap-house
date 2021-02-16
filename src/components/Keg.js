82c6acc0282305a5fef6f451cfdf35b2062e26bd
Tawnee Harris <tawneeh@icloud.com>
2021-02-15 20:14:51 -0800

    add string interplation

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,11 +4,13 @@ import PropTypes from "prop-types";
 function Keg(props) {
   return (
     <>
-      <h3>props.name</h3>
-      <h3>props.brand</h3>
-      <h3>props.price</h3>
-      <h3>props.alcoholContent</h3>
-      <h3>props.pintsLeft</h3>
+      <div onClick = { () => props.whenKegClicked(props.id) }>
+        <h3>{props.name}</h3>
+        <h3>{props.brand}</h3>
+        <h3>{props.price}</h3>
+        <h3>{props.alcoholContent}</h3>
+        <h3>{props.pintsLeft}</h3>
+      </div>
     </>
   );
 }
@@ -18,7 +20,8 @@ Keg.propTypes = {
   brand: PropTypes.string,
   price: PropTypes.string, 
   alcoholContent: PropTypes.number,
-  pintsLeft: PropTypes.number
+  pintsLeft: PropTypes.number,
+  whenKegClicked: PropTypes.func
 };
 
 export default Keg;
