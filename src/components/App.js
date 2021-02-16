ed5177b2e6f38a64ba87da438676892b7efb9633
Tawnee Harris <tawneeh@icloud.com>
2021-02-15 21:20:06 -0800

    add TicketControl to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import KegControl from "./KegControl";
 
 function App() {
   return (
     <>
       <Header />
+      <KegControl />
     </>
   );
 }
